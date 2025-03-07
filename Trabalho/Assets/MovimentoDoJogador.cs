2cf0b369989aaa583d4515af6fc793b81ddd9d2a
Gabriel Pedro <168735938+pkzinhogabriel@users.noreply.github.com>
2025-03-07 08:53:03 -0300

    Pulo

--- Trabalho/Assets/MovimentoDoJogador.cs
+++ Trabalho/Assets/MovimentoDoJogador.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class MovimentoDoJogador : MonoBehaviour
 {
     private Rigidbody2D rigidbody2d;
-    public float velocidadeDoJogador =3.0f ;
+    public float velocidadeDoJogador =2.0f ;
+    public float jumpForce = 10.0f;
+    public Transform GroundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+    public float moveInput;
     // Start is called before the first frame update
     
     void Start()
@@ -16,9 +22,18 @@ public class MovimentoDoJogador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, groundCheckRadius, groundLayer);
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(isGrounded)
+            {
+                rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x,jumpForce);
+            }
+        }
         float horizontal = Input.GetAxis("Horizontal");
        
         rigidbody2d.velocity = new Vector2(horizontal * velocidadeDoJogador, rigidbody2d.velocity.y);
     }
-    
+   
+   
 }
